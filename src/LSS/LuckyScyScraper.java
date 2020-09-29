c12aee221229803f7600f62fba9e75aba21b1feb
macbook <uzunic.e@gmail.com>
2020-09-29 10:59:27 +0200

    Added a loop for realFloors

--- src/LSS/LuckyScyScraper.java
+++ src/LSS/LuckyScyScraper.java
@@ -2,4 +2,10 @@ package LSS;
 
 public class LuckyScyScraper {
 
+    static public void floorList(int totalRealFloors){
+        //loop through real floor numbers
+        for(var realFloor = 1; realFloor <= totalRealFloors; realFloor++){
+            System.out.println("realFloorNo: " + realFloor);
+        }
+    }
 }
