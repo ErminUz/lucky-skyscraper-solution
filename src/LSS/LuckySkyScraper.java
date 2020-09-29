22192e0e019b1146fa23a51b5d7df2ba08d3fdb7
macbook <uzunic.e@gmail.com>
2020-09-29 11:45:32 +0200

    Completed the 2 methods that make up the class: giving you a real floor number from a fake and vice versa

--- src/LSS/LuckySkyScraper.java
+++ src/LSS/LuckySkyScraper.java
@@ -25,4 +25,20 @@ public class LuckySkyScraper {
         }
         return -1;
     }
+
+    static public int roFakeFloor(int realFloorGoal){
+        var fakeFloor = 1;
+        for(var realFloor = 1; realFloor < realFloorGoal; realFloor++){
+            /*
+            if(realFloorGoal == fakeFloor){
+                return fakeFloor;
+            }
+            */
+            do {
+                fakeFloor++;
+            } while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
+        }
+        return fakeFloor;
+        //return -1;
+    }
 }
--- src/LSS/Main.java
+++ src/LSS/Main.java
@@ -2,6 +2,13 @@ package LSS;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LuckySkyScraper.roRealFloor(15));
+        //System.out.println(LuckySkyScraper.roRealFloor(15));
+        for(var floor = 1; floor <= 220; floor++){
+            System.out.print("Floor: " + floor);
+            var fakeFloor = LuckySkyScraper.roFakeFloor(floor);
+            System.out.print(" Fakefloor: " + floor);
+            var backToRealFloor = LuckySkyScraper.roRealFloor(fakeFloor);
+            System.out.println(" Back to real " + backToRealFloor);
+        }
     }
 }
